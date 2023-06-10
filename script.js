684a1b3ed41b5418cdc303d5f72e9c1abbb1ead1
Avineet Bhat <surajbhat316@gmail.com>
2023-06-10 22:00:17 +0530

    Added Comments

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+/***
+ * 
+ * Created by : Avineet Bhat
+ * Created Date : 9th June 2015
+ */
+
+
 const addTaskBtn = document.getElementById('add-task');
 const tasksList = document.getElementById('list');
 const removeBtn = document.getElementById("remove-icon");
 let tasks = [];
 let totalTasks = document.getElementById('total-tasks');
-let count = 0;
+let count = 0;//To keep a count of number of tasks
 
+
+
+/**
+ * 
+ * This method whenever called iterates over the tasks list and 
+ * creates a new list item for that task and append it to the 
+ * ul tag in the HTML side
+ */
 function renderTaskListItem(tasks){
     tasksList.innerHTML = "";
     for(let task of tasks){
@@ -28,6 +43,17 @@ function renderTaskListItem(tasks){
 }
 
 
+/**
+ * This method takes the value from the input tag
+ * Creates a new JSON object for task 
+ * And adds the JSON Objext to the list of tasks
+ * 
+ * If the user does not enter the value then a corresponding
+ * alert is shown
+ * 
+ * Once the task is added to the list of tasks the renderTaskListItem method is called
+ * which renders the list of tasks on the screen
+ */
 function createTask(){
     const inputElem = document.getElementById("inputElement");
     if(inputElem.value){
@@ -47,6 +73,11 @@ function createTask(){
     alert("Please enter the task name");
 }
 
+
+/**
+ * This method calls the createTask method which 
+ * contains the logic for creating a new List Item
+ */
 function handleAddTask(){
     createTask();
 }
@@ -73,6 +104,9 @@ function handleDeleteOperation(e){
         renderTaskListItem(tasks);
     }
 }
+
+//Event Listner for the add button click
+// Once Clicked the handleAddTask method will be called 
 addTaskBtn.addEventListener('click', handleAddTask);
 document.addEventListener('click', handleDeleteOperation);
 
